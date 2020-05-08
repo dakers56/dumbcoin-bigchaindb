52be50e061e9a7616009a95ee5c64efc460ca350
Devon Akers <devonpa7@gmail.com>
2020-05-07 18:15:54 -0700

    Created scripts to start & stop bigchaindb

---
+++ run-bigchaindb.sh
@@ -0,0 +1,13 @@
+# !/bin/zsh
+
+docker run \
+  --detach \
+  --name bigchaindb \
+  --publish 9984:9984 \
+  --publish 9985:9985 \
+  --publish 27017:27017 \
+  --publish 26657:26657 \
+  --volume $HOME/bigchaindb_docker/mongodb/data/db:/data/db \
+  --volume $HOME/bigchaindb_docker/mongodb/data/configdb:/data/configdb \
+  --volume $HOME/bigchaindb_docker/tendermint:/tendermint \
+  bigchaindb/bigchaindb:all-in-one &
